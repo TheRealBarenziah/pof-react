c31b36c8d4250f2718fb78559df502ffa0cbe55a
Barenziah <presciente.snotrq@gmail.com>
2019-03-29 00:21:08 +0100

    Added kittens and stuff.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/header'
 import Body from './components/body/body'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Container, Col } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
             < Header />
+            < Body />
+            < Body />
             < Body />
       </div>
     );
